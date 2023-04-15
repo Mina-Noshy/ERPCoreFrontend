9b3a977be68cd579a729cce5ce906f4b557a26f8
Mina Noshy <mina-noshy@outlook.com>
2023-04-15 11:07:47 +0200

    update routings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,23 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { DashboardComponent } from './components/layout/dashboard/dashboard.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { MatrialImportsModule } from './modules/matrial-imports/matrial-imports.module';
+import { CategoryIndexComponent } from './components/categories/category-index/category-index.component';
+import { CategoryFormComponent } from './components/categories/category-form/category-form.component';
+import { ProductFormComponent } from './components/products/product-form/product-form.component';
+import { ProductIndexComponent } from './components/products/product-index/product-index.component';
+import { HomeComponent } from './components/layout/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     DashboardComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    CategoryIndexComponent,
+    CategoryFormComponent,
+    ProductFormComponent,
+    ProductIndexComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/modules/routings/routings.module.ts
+++ src/app/modules/routings/routings.module.ts
@@ -2,13 +2,36 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from 'src/app/pages/not-found/not-found.component';
+import { CategoryIndexComponent } from 'src/app/components/categories/category-index/category-index.component';
+import { ProductIndexComponent } from 'src/app/components/products/product-index/product-index.component';
+import { DashboardComponent } from 'src/app/components/layout/dashboard/dashboard.component';
 import { NavigationComponent } from 'src/app/components/layout/navigation/navigation.component';
+import { HomeComponent } from 'src/app/components/layout/home/home.component';
 
 const routes: Routes =
   [
     {
       path: '',
-      component: NavigationComponent
+      component: NavigationComponent,
+      children: 
+      [
+        {
+          path: '',
+          component: HomeComponent
+        },
+        {
+          path: 'dashboard',
+          component: DashboardComponent
+        },
+        {
+          path: 'categories',
+          component: CategoryIndexComponent
+        },
+        {
+          path: 'products',
+          component: ProductIndexComponent
+        },
+      ]
     },
     {
       path: 'not-found',
